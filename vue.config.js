e4ac2f8b45c1913975286f13a2f13af5ccc5e70b
Amirali Golru <golruamirali@gmail.com>
2022-01-28 16:47:18 +0330

    fix build probliem try1

--- vue.config.js
+++ vue.config.js
@@ -10,12 +10,7 @@ module.exports = {
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
+    
   },
   transpileDependencies: [
     'vuetify'
