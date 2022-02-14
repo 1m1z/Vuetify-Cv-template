7c087cd97e30dc91f7e178ef173c221809ff8605
Amirali Golru <golruamirali@gmail.com>
2022-02-14 20:58:50 +0330

    pwa try2

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   pwa: {
     name: 'Amirali Golru',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    },
     icon: [
       {
         "src": "/src/assets/Logo/pwa/code1",
@@ -17,6 +27,8 @@ module.exports = {
         'sizes': '512x512',
         'type': 'image/png',
       },
+      
+      
     ],
     
   },
