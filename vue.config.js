c4f453315dc0e449d40b2cc828b37a7750958a5c
Amirali Golru <golruamirali@gmail.com>
2022-02-16 00:13:57 +0330

    pwa try 9

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,38 @@
 module.exports = {
+  pwa: {
+    name: 'Amirali Golru',
+    shortName:'Amir Web App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxOptions: {
+    skipWaiting: true
+    },
+    iconPaths: {
+      favicon32: 'public/img/icons/favicon-32x32.png',
+   },
+    icon: [
+      {
+        "src": "src/assets/Logo/pwa/code1",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        'src': './src/assets/Logo/pwa/code2',
+        'sizes': '512x512',
+        'type': 'image/png',
+      },
+      {
+        'src': '@/src/assets/Logo/pwa/code2',
+        'sizes': '512x512',
+        'type': 'image/png',
+      },
+      
+      
+    ],
+    
+  },
   pluginOptions: {
     i18n: {
       locale: 'en',
