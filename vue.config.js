8c42e28d269f603e5876e7046c99e46c01766386
Amirali Golru <golruamirali@gmail.com>
2022-02-15 20:25:05 +0330

    fixing pwa try 5

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  pwa: {
-    name: 'Amirali Golru',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
+  // pwa: {
+  //   name: 'Amirali Golru',
+  //   themeColor: '#4DBA87',
+  //   msTileColor: '#000000',
+  //   appleMobileWebAppCapable: 'yes',
+  //   appleMobileWebAppStatusBarStyle: 'black',
     
-    icon: [
-      {
-        "src": "/src/assets/Logo/pwa/code1",
-        "sizes": "192x192",
-        "type": "image/png"
-      },
-      {
-        'src': './src/assets/Logo/pwa/code2',
-        'sizes': '512x512',
-        'type': 'image/png',
-      },
-      {
-        'src': '@/src/assets/Logo/pwa/code2',
-        'sizes': '512x512',
-        'type': 'image/png',
-      },
+  //   icon: [
+  //     {
+  //       "src": "/src/assets/Logo/pwa/code1",
+  //       "sizes": "192x192",
+  //       "type": "image/png"
+  //     },
+  //     {
+  //       'src': './src/assets/Logo/pwa/code2',
+  //       'sizes': '512x512',
+  //       'type': 'image/png',
+  //     },
+  //     {
+  //       'src': '@/src/assets/Logo/pwa/code2',
+  //       'sizes': '512x512',
+  //       'type': 'image/png',
+  //     },
       
       
-    ],
+  //   ],
     
-  },
+  // },
 
   pluginOptions: {
     i18n: {
