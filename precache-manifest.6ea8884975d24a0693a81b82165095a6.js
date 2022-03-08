409837d6068e7f1255f1712b54072a0a76178e04
Amirali Golru <golruamirali@gmail.com>
2022-03-08 15:29:48 +0330

    New Deplyment

---
+++ precache-manifest.6ea8884975d24a0693a81b82165095a6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7ad8d0222b88ec07a5",
+    "url": "/Vuetify-Cv-template/css/app.4efca623.css"
+  },
+  {
+    "revision": "622656302c4a1690d8e8",
+    "url": "/Vuetify-Cv-template/css/chunk-vendors.e6c88682.css"
+  },
+  {
+    "revision": "d5069810cb79c63a817a56319f231115",
+    "url": "/Vuetify-Cv-template/img/15.d5069810.png"
+  },
+  {
+    "revision": "9eca9293eb0f993ba6527f1753248bf3",
+    "url": "/Vuetify-Cv-template/img/20.9eca9293.png"
+  },
+  {
+    "revision": "21ffcf0b66f138c4327736d846fe0e2c",
+    "url": "/Vuetify-Cv-template/img/23.21ffcf0b.png"
+  },
+  {
+    "revision": "6c57715a463537d6623c39e37c30b1ae",
+    "url": "/Vuetify-Cv-template/img/26.6c57715a.png"
+  },
+  {
+    "revision": "5b2da4e089c474e21d8f0a688f3b4502",
+    "url": "/Vuetify-Cv-template/img/404notfound.5b2da4e0.jpg"
+  },
+  {
+    "revision": "0706a8aae5657858bd20f4272e3ab4b6",
+    "url": "/Vuetify-Cv-template/img/Face2-2.0706a8aa.png"
+  },
+  {
+    "revision": "3288f74abf652a8251abe05751f21aac",
+    "url": "/Vuetify-Cv-template/img/Face2.3288f74a.png"
+  },
+  {
+    "revision": "ec352c97a6a5dc3b5ee9d7ca029adcdd",
+    "url": "/Vuetify-Cv-template/img/amiralig.ec352c97.png"
+  },
+  {
+    "revision": "0d64ceb2fcdfa7186958910ecd37bf25",
+    "url": "/Vuetify-Cv-template/img/hekfa.0d64ceb2.png"
+  },
+  {
+    "revision": "0ee563409e01a301759279a5805e8770",
+    "url": "/Vuetify-Cv-template/img/royalvision.0ee56340.png"
+  },
+  {
+    "revision": "161f360476e1ac6e11290d7239fb1aa5",
+    "url": "/Vuetify-Cv-template/img/vue.161f3604.jpeg"
+  },
+  {
+    "revision": "5e207b41ae78e363d0afdfe61c8b749f",
+    "url": "/Vuetify-Cv-template/img/vuejs.5e207b41.jpg"
+  },
+  {
+    "revision": "b4434cfd9c5f89be2dc28d1270004718",
+    "url": "/Vuetify-Cv-template/index.html"
+  },
+  {
+    "revision": "ab7ad8d0222b88ec07a5",
+    "url": "/Vuetify-Cv-template/js/app.9528df5a.js"
+  },
+  {
+    "revision": "622656302c4a1690d8e8",
+    "url": "/Vuetify-Cv-template/js/chunk-vendors.b3fcf85e.js"
+  },
+  {
+    "revision": "e1c035486f5ad2795a5aa03b0b1b955f",
+    "url": "/Vuetify-Cv-template/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vuetify-Cv-template/robots.txt"
+  }
+]);
